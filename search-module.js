ebd0349b1ead59b39bf8581949b759762e74bc99
jungjieun <je_eun2735@naver.com>
2023-03-29 12:08:35 +0900

    search함수 모듈화

--- search-module.js
+++ search-module.js
@@ -1,7 +1,22 @@
-for(let i =0; i< studentList.length; i++){
-  if(studentList[i]==="정지은"){
-    console.log(i);
-  } else{
-    console.log("해당 순서는 정지은씨가 아닙니다.");
+module.exports = function searching(array, name){
+  for(let i =0; i< studentList.length; i++){
+    if(studentList[i]==="name"){
+      console.log(i);
+    }else {
+      console.log(`해당 순서는 ${name}이 아닙니다.`)
+    }
   }
-}
+} 
+//commonJS 방식
+
+// export default  function searching(array, name){
+//   for(let i =0; i< studentList.length; i++){
+//     if(studentList[i]==="name"){
+//       console.log(i);
+//     }else {
+//       console.log(`해당 순서는 ${name}이 아닙니다.`)
+//     }
+//   }
+// } 
+
+//esm방식
