62b428856f882ae468639c08a34974afca5914bd
jungjieun <je_eun2735@naver.com>
2023-03-29 10:45:25 +0900

    생성자 함수

--- app1.js
+++ app1.js
@@ -1,7 +1,20 @@
 function c(year,month,day) {
-  return {
-    year : year,
-    month : month,
-    day : day,
-  };
+  //return을 객체로 주었다.
+  
+    this.year = year;
+    this.month = month;
+    this.day = day;
 }
+
+// const test = c(2023,3,29).year; //객체를 리턴했기 때문에 .year 이 가능해졌다.
+// console.log(test); //? 2023 
+
+function user(id, password, email){
+  this.id = id,
+  this.password = password,
+  this.email = email
+};
+
+const d = new user("jieun","1234","je_eun2735@naver.com");
+console.log(d); 
+//틀을 만들어놓고 객체를 찍어내자 = 생성자 함수를 사용
